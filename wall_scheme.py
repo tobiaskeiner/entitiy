5fa215b17f328b8374b3440281c8673796cf9d33
Fabian Wunsch <fabian@uriah.heep.sax.de>
2021-10-23 10:53:10 +0200

    higher walls

--- wall_scheme.py
+++ wall_scheme.py
@@ -2,17 +2,21 @@
 from typing import List, Tuple
 
 # multiplier for coordinates
-X_MULT: float = 1
-Y_MULT: float = 1
+X_MULT: float = 1.25
+Y_MULT: float = 1.25
 
 # every point will get shifted by these values
 X_OFFSET: int = 45
-Y_OFFSET: int = 73
+Y_OFFSET: int = 74
 
 # positive limit for coordinates
 # negative values will be discarded
 X_LIMIT: int = 90
-Y_LIMIT: int = 25
+Y_LIMIT: int = 26
+
+# parameters for walls
+IS_WALL = True
+WALL_HEIGHT = 2
 
 # input file
 FILE: str = 'merged'
@@ -25,7 +29,7 @@ sf: shapefile.Reader = shapefile.Reader(FILE)
 X_MAX: int = round(X_LIMIT * X_MULT)
 Y_MAX: int = round(Y_LIMIT * Y_MULT)
 
-grid: List[List[bool]] = [[False for _ in range(X_MAX)] for _ in range(Y_MAX)]
+grid: List[List[bool]] = [[0 for _ in range(X_MAX)] for _ in range(Y_MAX + WALL_HEIGHT if IS_WALL else Y_MAX)]
 
 for shape in sf.iterShapes():
     coords: List[float] = shape.bbox
@@ -42,15 +46,25 @@ for shape in sf.iterShapes():
     ):
         points: List[Tuple[int, int]] = get_grid((start_x, start_y), (end_x, end_y))
         for x, y in points:
-            grid[y][x] = True
+            grid[y][x] = 1
 
-#print(grid)
+
+if IS_WALL:
+    basis = [line.copy() for line in grid]
+    for i in range(1, WALL_HEIGHT + 1):
+        for y, line in enumerate(basis):
+            for x, tile in enumerate(line):
+                if tile != 0 and grid[y + i][x] == 0:
+                    grid[y + i][x] = 2 if WALL_HEIGHT == i else tile
+
 grid.reverse()
 
 for array in grid:
     for element in array:
-        if element == True:
-            print("#",end='')
+        if element == 1:
+            print("█", end='')
+        elif element == 2:
+            print('▄', end='')
         else:
-            print(".",end='')
+            print(" ",end='')
     print()
